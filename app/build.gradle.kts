e321ef992d29f8120eeb815f4a176d55f8d9a756
Antoine <pirlotantoine2@gmail.com>
2024-01-17 12:58:30 +0100

    Added new dependencies for media3 api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,13 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    //Media
+    implementation("androidx.media3:media3-exoplayer:1.2.1")
+    implementation("androidx.media3:media3-session:1.2.1")
+    implementation("androidx.media3:media3-ui:1.2.1")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")

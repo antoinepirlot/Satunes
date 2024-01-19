cac01b5d347bf98f627436a98944bd4a98ce8991
Antoine <pirlotantoine2@gmail.com>
2024-01-19 23:35:55 +0100

    Refactor: move loadmusic to Music.kt

--- app/src/main/java/earth/mp3/MainActivity.kt
+++ app/src/main/java/earth/mp3/MainActivity.kt
@@ -1,11 +1,8 @@
 package earth.mp3
 
 import android.Manifest.permission.READ_MEDIA_AUDIO
-import android.content.Context
 import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.ActivityResultLauncher
@@ -19,6 +16,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import earth.mp3.models.Music
+import earth.mp3.models.loadMusics
 import earth.mp3.ui.theme.MP3Theme
 import earth.mp3.ui.views.HomeView
 
@@ -40,45 +38,6 @@ class MainActivity : ComponentActivity() {
                         HomeView(modifier = Modifier, musicList = musicList)
                     }
                 }
-            }
-        }
-    }
-
-    private fun loadMusics(context: Context, musicList: MutableList<Music>) {
-        val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-        val projection = arrayOf(
-            MediaStore.Audio.Media._ID,
-            MediaStore.Audio.Media.DISPLAY_NAME,
-            MediaStore.Audio.Media.DURATION,
-            MediaStore.Audio.Media.SIZE,
-            MediaStore.Audio.Media.RELATIVE_PATH
-        )
-
-        context.contentResolver.query(
-            uri, projection, null, null
-        )?.use { cursor ->
-            // Cache column indices.
-            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
-            val nameColumn =
-                cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME)
-            val durationColumn =
-                cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)
-            val sizeColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.SIZE)
-            val relativePathColumn =
-                cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.RELATIVE_PATH)
-
-            while (cursor.moveToNext()) {
-                // Get values of columns for a given video.
-                val id = cursor.getLong(idColumn)
-                val name = cursor.getString(nameColumn)
-                val duration = cursor.getInt(durationColumn)
-                val size = cursor.getInt(sizeColumn)
-                val relativePath = cursor.getString(relativePathColumn)
-
-                // Stores column values and the contentUri in a local object
-                // that represents the media file.
-                val fileUri = Uri.Builder().appendPath("${uri.path}/${name}").build()
-                musicList.add(Music(id, name, duration, size, fileUri, relativePath))
             }
         }
     }
--- app/src/main/java/earth/mp3/models/Music.kt
+++ app/src/main/java/earth/mp3/models/Music.kt
@@ -1,6 +1,8 @@
 package earth.mp3.models
 
+import android.content.Context
 import android.net.Uri
+import android.provider.MediaStore
 
 
 data class Music(
@@ -11,6 +13,45 @@ data class Music(
     val uri: Uri?,
     val relativePath: String
 )
+
+fun loadMusics(context: Context, musicList: MutableList<Music>) {
+    val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+    val projection = arrayOf(
+        MediaStore.Audio.Media._ID,
+        MediaStore.Audio.Media.DISPLAY_NAME,
+        MediaStore.Audio.Media.DURATION,
+        MediaStore.Audio.Media.SIZE,
+        MediaStore.Audio.Media.RELATIVE_PATH
+    )
+
+    context.contentResolver.query(
+        uri, projection, null, null
+    )?.use { cursor ->
+        // Cache column indices.
+        val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+        val nameColumn =
+            cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME)
+        val durationColumn =
+            cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)
+        val sizeColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.SIZE)
+        val relativePathColumn =
+            cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.RELATIVE_PATH)
+
+        while (cursor.moveToNext()) {
+            // Get values of columns for a given video.
+            val id = cursor.getLong(idColumn)
+            val name = cursor.getString(nameColumn)
+            val duration = cursor.getInt(durationColumn)
+            val size = cursor.getInt(sizeColumn)
+            val relativePath = cursor.getString(relativePathColumn)
+
+            // Stores column values and the contentUri in a local object
+            // that represents the media file.
+            val fileUri = Uri.Builder().appendPath("${uri.path}/${name}").build()
+            musicList.add(Music(id, name, duration, size, fileUri, relativePath))
+        }
+    }
+}
 
 fun getFolderList(musicList: List<Music>): MutableMap<String, Folder> {
     val folderMap = mutableMapOf<String, Folder>()

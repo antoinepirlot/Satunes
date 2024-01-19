b225b52a7aa9f899c7015af675795cb49e9808e9
Antoine <pirlotantoine2@gmail.com>
2024-01-19 23:36:31 +0100

    Refactor: move getFolderList to Folder.kt

--- app/src/main/java/earth/mp3/models/Folder.kt
+++ app/src/main/java/earth/mp3/models/Folder.kt
@@ -5,3 +5,21 @@ data class Folder(
     val subFolders: MutableMap<String, Folder> = mutableMapOf(),
     val musicList: MutableList<Music> = mutableListOf()
 )
+
+fun getFolderList(musicList: List<Music>): MutableMap<String, Folder> {
+    val folderMap = mutableMapOf<String, Folder>()
+    musicList.forEach { music: Music ->
+        if (folderMap.containsKey(music.relativePath)) {
+            //Add music to the right folder
+            folderMap[music.relativePath]!!.musicList.add(music)
+        } else {
+            val newFolder = Folder(
+                relativePath = music.relativePath,
+                musicList = mutableListOf(music)
+            )
+            folderMap[music.relativePath] = newFolder
+        }
+
+    }
+    return folderMap
+}
--- app/src/main/java/earth/mp3/models/Music.kt
+++ app/src/main/java/earth/mp3/models/Music.kt
@@ -52,21 +52,3 @@ fun loadMusics(context: Context, musicList: MutableList<Music>) {
         }
     }
 }
-
-fun getFolderList(musicList: List<Music>): MutableMap<String, Folder> {
-    val folderMap = mutableMapOf<String, Folder>()
-    musicList.forEach { music: Music ->
-        if (folderMap.containsKey(music.relativePath)) {
-            //Add music to the right folder
-            folderMap[music.relativePath]!!.musicList.add(music)
-        } else {
-            val newFolder = Folder(
-                relativePath = music.relativePath,
-                musicList = mutableListOf(music)
-            )
-            folderMap[music.relativePath] = newFolder
-        }
-
-    }
-    return folderMap
-}

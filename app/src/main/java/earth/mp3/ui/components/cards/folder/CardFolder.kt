db6bf9686b617d69368dbb7b084e9ca4d4337899
Antoine <pirlotantoine2@gmail.com>
2024-01-21 13:29:52 +0100

    Update: using list item for card folder

--- app/src/main/java/earth/mp3/ui/components/cards/folder/CardFolder.kt
+++ app/src/main/java/earth/mp3/ui/components/cards/folder/CardFolder.kt
@@ -1,6 +1,10 @@
 package earth.mp3.ui.components.cards.folder
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -10,14 +14,26 @@ import androidx.compose.ui.tooling.preview.Preview
 @Composable
 fun CardFolder(
     modifier: Modifier = Modifier,
-    folderName: String
+    folderName: String,
+    onClick: () -> Unit
 ) {
-    TextButton(
-        modifier = modifier,
-        onClick = { /*TODO*/ } //redirect to the folder's view
-    ) {
-        Text(text = folderName)
-    }
+    ListItem(
+        headlineContent = {
+            TextButton(
+                modifier = modifier,
+                onClick = onClick //redirect to the folder's view
+            ) {
+                Text(text = folderName)
+            }
+        },
+        leadingContent = {
+            Icon(
+                Icons.Filled.KeyboardArrowRight,
+                contentDescription = "Arrow Right"
+            )
+        }
+    )
+
     Divider(modifier = modifier)
 }
 
@@ -25,5 +41,5 @@ fun CardFolder(
 @Preview
 fun CardFolderPreview() {
 
-    CardFolder(folderName = "Nom de dossier")
+    CardFolder(folderName = "Nom de dossier", onClick = {/* TODO */ })
 }

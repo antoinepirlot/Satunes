36f415c503693e181e1ab75e550f7cc93318e341
Antoine <pirlotantoine2@gmail.com>
2024-01-22 23:42:17 +0100

    Remove: old function

--- app/src/main/java/earth/mp3/models/utils/DataStructure.kt
+++ app/src/main/java/earth/mp3/models/utils/DataStructure.kt
@@ -1,13 +1,5 @@
 package earth.mp3.models.utils
 
-fun <T, U> mapToList(map: Map<U, T>): List<T> {
-    val listToReturn = mutableListOf<T>()
-    map.map { entry ->
-        listToReturn.add(entry.value)
-    }
-    return listToReturn
-}
-
 fun <T> loadObjectsTo(toList: MutableList<T>, fromList: List<T>) {
     toList.clear()
     for (obj: T in fromList) {

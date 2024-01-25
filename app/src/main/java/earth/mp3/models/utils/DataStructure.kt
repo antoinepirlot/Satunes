76caa1029232ce702e5f247ff579780d11869628
Antoine <pirlotantoine2@gmail.com>
2024-01-25 14:27:28 +0100

    Add: fun to load from map

--- app/src/main/java/earth/mp3/models/utils/DataStructure.kt
+++ app/src/main/java/earth/mp3/models/utils/DataStructure.kt
@@ -2,7 +2,10 @@ package earth.mp3.models.utils
 
 fun <T> loadObjectsTo(toList: MutableList<T>, fromList: List<T>) {
     toList.clear()
-    for (obj: T in fromList) {
-        toList.add(obj)
-    }
+    toList.addAll(fromList)
+}
+
+fun <T, U> loadObjectsToMap(toList: MutableMap<T, U>, fromMap: Map<T, U>) {
+    toList.clear()
+    toList.putAll(fromMap)
 }

83af2c36772f13228d0b1be5b68c4d8a696414da
Antoine <pirlotantoine2@gmail.com>
2024-01-26 12:46:07 +0100

    Fix: the switch button

--- app/src/main/java/earth/mp3/ui/components/music/MusicControlBar.kt
+++ app/src/main/java/earth/mp3/ui/components/music/MusicControlBar.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -22,7 +23,10 @@ fun MusicControlBar(
     Row(modifier = modifier) {
         val isPlaying = rememberSaveable { mutableStateOf(mediaPlayerManager.isPlaying()) }
         IconButton(onClick = { playPause(mediaPlayerManager, isPlaying) }) {
-            if (isPlaying.value) {
+            Text(text = isPlaying.value.toString())
+            // TODO I used the inverse otherwise when is playing it is false
+            //  I think it's a issue with coroutine or something like this
+            if (!isPlaying.value) {
                 Icon(imageVector = Icons.Filled.Close, contentDescription = "Close")
             } else {
                 Icon(imageVector = Icons.Filled.PlayArrow, contentDescription = "Play Icon")

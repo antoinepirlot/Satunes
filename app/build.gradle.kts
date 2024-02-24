6f300a40070e00632784eec7be1787049bbce8c9
Antoine <pirlotantoine2@gmail.com>
2024-02-24 11:32:09 +0100

    Update: update version name 0.1.0-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 34
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "0.1.0-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

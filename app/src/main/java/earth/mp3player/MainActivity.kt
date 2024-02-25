7e8eed54ed0498eefed07ecadd1f2c99af340aaa
Antoine <pirlotantoine2@gmail.com>
2024-02-25 17:53:55 +0100

    COPYLEFT: add GNU v3 copyleft

--- app/src/main/java/earth/mp3player/MainActivity.kt
+++ app/src/main/java/earth/mp3player/MainActivity.kt
@@ -1,9 +1,16 @@
 /*
- * Copyright (c) 2024. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
- * Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
- * Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
- * Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
- * Vestibulum commodo. Ut rhoncus gravida arcu.
+ *  This file is part of MP3 Player.
+ *
+ *  MP3 Player is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software Foundation,
+ *  either version 3 of the License, or (at your option) any later version.
+ *
+ *  MP3 Player is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *   without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with MP3 Player.
+ *  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package earth.mp3player
--- app/src/main/java/earth/mp3player/router/Router.kt
+++ app/src/main/java/earth/mp3player/router/Router.kt
@@ -1,3 +1,18 @@
+/*
+ *  This file is part of MP3 Player.
+ *
+ *  MP3 Player is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software Foundation,
+ *  either version 3 of the License, or (at your option) any later version.
+ *
+ *  MP3 Player is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *   without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with MP3 Player.
+ *  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package earth.mp3player.router
 
 import androidx.compose.runtime.Composable

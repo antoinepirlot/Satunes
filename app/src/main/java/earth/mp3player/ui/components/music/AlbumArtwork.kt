e1330ce53934e7300917e800d2738c5fecc7bcd8
Antoine <pirlotantoine2@gmail.com>
2024-03-02 00:08:23 +0100

    Update: show album artwork on screen if no album artwork in music

--- app/src/main/java/earth/mp3player/ui/components/music/AlbumArtwork.kt
+++ app/src/main/java/earth/mp3player/ui/components/music/AlbumArtwork.kt
@@ -1,8 +1,14 @@
 package earth.mp3player.ui.components.music
 
+import android.net.Uri
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import earth.mp3player.R
 import earth.mp3player.services.PlaybackController
 
 @Composable
@@ -10,6 +16,14 @@ fun AlbumArtwork(
     modifier: Modifier = Modifier
 ) {
     val playbackController: PlaybackController = PlaybackController.getInstance()
+    val musicPlaying by remember { playbackController.musicPlaying }
+
+    if (musicPlaying!!.album == null || musicPlaying!!.album!!.albumArtWorkUri == Uri.EMPTY) {
+        Image(
+            painter = painterResource(id = R.mipmap.empty_album_artwork_foreground),
+            contentDescription = "Default Album Artwork"
+        )
+    }
 }
 
 @Composable

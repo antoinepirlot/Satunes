94f7fd83bfe6bfdc3209c433706a4797eacadf30
Antoine <pirlotantoine2@gmail.com>
2024-03-02 16:49:45 +0100

    Update: add albums option and optimize code by using map

--- app/src/main/java/earth/mp3player/ui/views/SettingsView.kt
+++ app/src/main/java/earth/mp3player/ui/views/SettingsView.kt
@@ -29,19 +29,21 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ListItem
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import earth.mp3player.R
+import earth.mp3player.models.MenuTitle
 
 /**
  * @author Antoine Pirlot on 02-03-24
@@ -52,38 +54,48 @@ fun SettingsView(
     modifier: Modifier = Modifier
 ) {
     // TODO move these check to main activity and change composition of router
-    var folderChecked by rememberSaveable { mutableStateOf(true) }
-    var artistChecked by rememberSaveable { mutableStateOf(true) }
+    val foldersChecked = rememberSaveable { mutableStateOf(true) }
+    val artistsChecked = rememberSaveable { mutableStateOf(true) }
+    val albumsChecked = rememberSaveable { mutableStateOf(true) }
+
+    val menuTitleCheckedMap: Map<MenuTitle, MutableState<Boolean>> = mapOf(
+        Pair(MenuTitle.FOLDERS, foldersChecked),
+        Pair(MenuTitle.ARTISTS, artistsChecked),
+        Pair(MenuTitle.ALBUMS, albumsChecked),
+    )
 
     Column(
         modifier = modifier
     ) {
-        Text(text = stringResource(id = R.string.bottom_bar))
-        ListItem(headlineContent = {
-            Row(
-                modifier = modifier.fillMaxWidth()
-            ) {
-                Text(text = stringResource(id = R.string.folders))
-                Switch(
-                    checked = folderChecked,
-                    onCheckedChange = { folderChecked = !folderChecked })
-            }
-        })
-        ListItem(headlineContent = {
-            Row(
-                modifier = modifier.fillMaxWidth()
-            ) {
-                Text(text = stringResource(id = R.string.artists))
-                Box(
-                    contentAlignment = Alignment.CenterEnd
-                ) {
-                    Switch(
-                        checked = artistChecked,
-                        onCheckedChange = { artistChecked = !artistChecked }
+        Box {
+            Text(text = stringResource(id = R.string.bottom_bar))
+            LazyColumn {
+                items(
+                    items = menuTitleCheckedMap.keys.toList(),
+                    key = { it.stringId }
+                ) { menuTitle: MenuTitle ->
+                    ListItem(
+                        headlineContent = {
+                            Row(
+                                modifier = modifier.fillMaxWidth()
+                            ) {
+                                Text(text = stringResource(id = menuTitle.stringId))
+                                Box(
+                                    contentAlignment = Alignment.CenterEnd
+                                ) {
+                                    val checked: MutableState<Boolean> =
+                                        menuTitleCheckedMap[menuTitle]!!
+                                    Switch(
+                                        checked = checked.value,
+                                        onCheckedChange = { checked.value = !checked.value }
+                                    )
+                                }
+                            }
+                        }
                     )
                 }
             }
-        })
+        }
     }
 }
 

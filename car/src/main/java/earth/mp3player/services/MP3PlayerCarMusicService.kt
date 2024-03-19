fe0c5d2ae6052cdfd6d31c4b00150214d24c0e4a
Antoine <pirlotantoine2@gmail.com>
2024-03-19 22:33:29 +0100

    Remove: remove map of media item and music
    Signed-off-by: Antoine <pirlotantoine2@gmail.com>

--- car/src/main/java/earth/mp3player/services/MP3PlayerCarMusicService.kt
+++ car/src/main/java/earth/mp3player/services/MP3PlayerCarMusicService.kt
@@ -26,13 +26,10 @@
 package earth.mp3player.services
 
 import android.annotation.SuppressLint
-import android.app.PendingIntent
-import android.content.Intent
 import android.os.Bundle
 import android.support.v4.media.MediaBrowserCompat.MediaItem
 import android.support.v4.media.session.MediaSessionCompat
 import androidx.media.MediaBrowserServiceCompat
-import androidx.media.session.MediaButtonReceiver
 import earth.mp3player.models.Album
 import earth.mp3player.models.Artist
 import earth.mp3player.models.Genre
@@ -55,7 +52,6 @@ class MP3PlayerCarMusicService : MediaBrowserServiceCompat() {
     companion object {
         val routeDeque: RouteDeque = RouteDeque()
         lateinit var session: MediaSessionCompat
-        val musicMediaItemMap: MutableMap<Music, MediaItem> = mutableMapOf()
     }
 
     override fun onCreate() {
@@ -182,9 +178,6 @@ class MP3PlayerCarMusicService : MediaBrowserServiceCompat() {
             }
 
             val mediaItem: MediaItem = buildMediaItem(media = media)
-            if (media is Music) {
-                musicMediaItemMap[media] = mediaItem
-            }
             mediaItemList.add(mediaItem)
         }
         return mediaItemList

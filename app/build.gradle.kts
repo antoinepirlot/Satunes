458dc5ad1b0a96623d7520ca29c6663c117218e8
Antoine <pirlotantoine2@gmail.com>
2024-03-24 13:25:44 +0100

    Update: check permission for Android 12 Snow Cone, app is now compatible with Android 12 Snow Cone

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         applicationId = nameSpace
-        minSdk = 32
+        minSdk = 31
         targetSdk = 34
         versionCode = 10
         versionName = "U.0.5.1-beta"

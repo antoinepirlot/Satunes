ec0788e44187f281e07a74c504bf858a46f3863c
Antoine <pirlotantoine@etik.com>
2024-03-27 15:59:27 +0100

    Add: init database

--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -40,4 +40,9 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    val room_version = "2.6.1"
+
+    implementation("androidx.room:room-runtime:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
 }

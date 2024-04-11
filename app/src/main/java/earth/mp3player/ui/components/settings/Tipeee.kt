15058d3ec3ccd64bb7ea487947746447717de812
Antoine <pirlotantoine2@gmail.com>
2024-04-11 10:01:59 +0200

    Refactor: extract Image with link to make it with general design

--- app/src/main/java/earth/mp3player/ui/components/settings/Tipeee.kt
+++ app/src/main/java/earth/mp3player/ui/components/settings/Tipeee.kt
@@ -25,51 +25,22 @@
 
 package earth.mp3player.ui.components.settings
 
-import android.content.Context
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.combinedClickable
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.hapticfeedback.HapticFeedbackType
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalHapticFeedback
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import earth.mp3player.R
-import earth.mp3player.ui.components.settings.utils.openUrl
-import earth.mp3player.ui.utils.showToast
+import earth.mp3player.ui.components.images.ImageWithLink
 
 /**
  * @author Antoine Pirlot on 10/04/2024
  */
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun Tipeee(
     modifier: Modifier = Modifier
 ) {
-    val haptics = LocalHapticFeedback.current
-    val context: Context = LocalContext.current
-    val tipeeeUrl: String = "https://fr.tipeee.com/antoinepirlot"
-    Image(
-        modifier = modifier
-            .size(60.dp)
-            .clip(RoundedCornerShape(20.dp))
-            .combinedClickable(
-                onClick = { openUrl(context = context, url = tipeeeUrl) },
-                onLongClick = {
-                    haptics.performHapticFeedback(HapticFeedbackType.LongPress)
-                    showToast(context = context, message = tipeeeUrl)
-                }
-            ),
-        painter = painterResource(id = R.drawable.tipeee_logo),
-        contentDescription = "Tipeee Logo",
-    )
+    val tipeeeUrl = "https://fr.tipeee.com/antoinepirlot"
+    ImageWithLink(modifier = modifier, url = tipeeeUrl, painterId = R.drawable.tipeee_logo)
 }
 
 @Preview

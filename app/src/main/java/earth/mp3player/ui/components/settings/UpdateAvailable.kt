00d728c3bd3e548b72067c6f5eca877a13d57b11
Antoine <pirlotantoine2@gmail.com>
2024-04-14 13:05:17 +0200

    Update: try to center buttons

--- app/src/main/java/earth/mp3player/ui/components/settings/UpdateAvailable.kt
+++ app/src/main/java/earth/mp3player/ui/components/settings/UpdateAvailable.kt
@@ -36,6 +36,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -63,7 +64,7 @@ fun UpdateAvailable(
         verticalArrangement = Arrangement.Center
     ) {
         Text(text = stringResource(id = R.string.update_available))
-        Row {
+        Row(verticalAlignment = Alignment.CenterVertically) {
             SeeDetailsButton()
             Spacer(modifier = Modifier.size(SPACER_SIZE))
             val downloadStatus: APKDownloadStatus by remember { UpdateManager.downloadStatus }

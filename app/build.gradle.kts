3f22d8f80aa35d1f98bbbf35a6c65be1c4e39276
Antoine <pirlotantoine2@gmail.com>
2024-04-20 21:33:47 +0200

    Update: update compose bom from 2023.08.00 to 2024.04.00

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,7 +65,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
--- playback/build.gradle.kts
+++ playback/build.gradle.kts
@@ -39,7 +39,7 @@ dependencies {
      */
     implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.ui:ui-graphics")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.04.00"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")

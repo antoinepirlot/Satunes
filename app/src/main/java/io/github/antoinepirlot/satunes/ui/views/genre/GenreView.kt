aef6677cb2dbac42dbe41cdb84ffe46015940dd6
Antoine <pirlotantoine2@gmail.com>
2024-04-27 09:52:18 +0200

    Update: show album grid and add title for music list in genre view

--- app/src/main/java/io/github/antoinepirlot/satunes/ui/views/genre/GenreView.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/views/genre/GenreView.kt
@@ -26,13 +26,17 @@
 package io.github.antoinepirlot.satunes.ui.views.genre
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.media3.common.MediaItem
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -46,10 +50,12 @@ import io.github.antoinepirlot.satunes.playback.services.PlaybackController
 import io.github.antoinepirlot.satunes.router.utils.openCurrentMusic
 import io.github.antoinepirlot.satunes.router.utils.openMedia
 import io.github.antoinepirlot.satunes.ui.components.buttons.ExtraButton
+import io.github.antoinepirlot.satunes.ui.components.cards.albums.AlbumGrid
 import io.github.antoinepirlot.satunes.ui.components.texts.Title
 import io.github.antoinepirlot.satunes.ui.views.MediaListView
 import java.util.SortedMap
 import java.util.SortedSet
+import io.github.antoinepirlot.satunes.database.R as RDb
 
 /**
  * @author Antoine Pirlot on 01/04/2024
@@ -78,6 +84,18 @@ fun GenreView(
         musicMap.forEach { (music: Music, _: MediaItem) ->
             albumSet.add(music.album)
         }
+
+        AlbumGrid(
+            mediaList = albumSet.toList(),
+            onClick = { openMedia(navController = navController, media = it) }
+        )
+
+        Title(
+            modifier.padding(start = 16.dp),
+            text = stringResource(id = RDb.string.musics),
+            textAlign = TextAlign.Left,
+            fontSize = 20.sp
+        )
 
         MediaListView(
             mediaList = musicMap.keys.toList(),

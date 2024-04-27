d7783ae992523e8c6adf349262c94604cd48e1b2
Antoine <pirlotantoine2@gmail.com>
2024-04-27 08:33:00 +0200

    Fix: show empty album artwork even if the storage is too low to load it correctly

--- app/src/main/java/io/github/antoinepirlot/satunes/ui/components/cards/media/MediaCard.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/components/cards/media/MediaCard.kt
@@ -1,26 +1,26 @@
 /*
  * This file is part of Satunes.
  *
- *  Satunes is free software: you can redistribute it and/or modify it under
+ * Satunes is free software: you can redistribute it and/or modify it under
  *  the terms of the GNU General Public License as published by the Free Software Foundation,
- *  either version 3 of the License, or (at your option) any later version.
+ * either version 3 of the License, or (at your option) any later version.
  *
- *  Satunes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *  See the GNU General Public License for more details.
+ * Satunes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License along with Satunes.
- *  If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with Satunes.
+ * If not, see <https://www.gnu.org/licenses/>.
  *
- *  **** INFORMATIONS ABOUT THE AUTHOR *****
- *  The author of this file is Antoine Pirlot, the owner of this project.
- *  You find this original project on github.
+ * **** INFORMATIONS ABOUT THE AUTHOR *****
+ * The author of this file is Antoine Pirlot, the owner of this project.
+ * You find this original project on github.
  *
- *  My github link is: https://github.com/antoinepirlot
- *  This current project's link is: https://github.com/antoinepirlot/Satunes
+ * My github link is: https://github.com/antoinepirlot
+ * This current project's link is: https://github.com/antoinepirlot/MP3-Player
  *
- *  You can contact me via my email: pirlot.antoine@outlook.com
- *  PS: I don't answer quickly.
+ * You can contact me via my email: pirlot.antoine@outlook.com
+ * PS: I don't answer quickly.
  */
 
 package io.github.antoinepirlot.satunes.ui.components.cards.media
@@ -45,12 +45,15 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.drawable.toBitmap
 import io.github.antoinepirlot.satunes.database.models.Album
 import io.github.antoinepirlot.satunes.database.models.Artist
 import io.github.antoinepirlot.satunes.database.models.Folder
@@ -60,6 +63,7 @@ import io.github.antoinepirlot.satunes.database.models.Music
 import io.github.antoinepirlot.satunes.database.models.relations.PlaylistWithMusics
 import io.github.antoinepirlot.satunes.database.models.tables.MusicDB
 import io.github.antoinepirlot.satunes.database.services.DatabaseManager
+import io.github.antoinepirlot.satunes.icons.R
 import io.github.antoinepirlot.satunes.icons.SatunesIcons
 import io.github.antoinepirlot.satunes.services.MediaSelectionManager
 import io.github.antoinepirlot.satunes.ui.components.dialog.MusicOptionsDialog
@@ -149,14 +153,24 @@ fun MediaCard(
                             contentDescription = "Media Artwork"
                         )
                     } else {
-                        val mediaIcon: SatunesIcons = getRightIconAndDescription(media = media)
-                        Icon(
-                            modifier = Modifier
-                                .size(30.dp)
-                                .align(Alignment.Center),
-                            imageVector = mediaIcon.imageVector,
-                            contentDescription = mediaIcon.description
-                        )
+                        if (media is Music || media is Album) {
+                            //Use it will prevent slow devices showing icon instead of artwork
+                            val emptyArtwork: ImageBitmap = ResourcesCompat.getDrawable(
+                                LocalContext.current.resources,
+                                R.mipmap.empty_album_artwork_foreground,
+                                null
+                            )?.toBitmap()!!.asImageBitmap()
+                            Image(bitmap = emptyArtwork, contentDescription = "Empty Album")
+                        } else {
+                            val mediaIcon: SatunesIcons = getRightIconAndDescription(media = media)
+                            Icon(
+                                modifier = Modifier
+                                    .size(30.dp)
+                                    .align(Alignment.Center),
+                                imageVector = mediaIcon.imageVector,
+                                contentDescription = mediaIcon.description
+                            )
+                        }
                     }
                 }
             }

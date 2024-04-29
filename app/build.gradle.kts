b5b8e10e787e887fee8978047b7ef0460aea4028
Antoine <pirlotantoine2@gmail.com>
2024-04-29 21:20:23 +0200

    Update: use variable to select version of compose ui and set it to 1.6.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,14 +86,15 @@ dependencies {
     /**
      * Base
      */
+    val composeUiVersion = "1.6.6"
     implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.0")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation(platform("androidx.compose:compose-bom:2024.04.01"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.ui:ui:$composeUiVersion")
+    implementation("androidx.compose.ui:ui-graphics:$composeUiVersion")
+    implementation("androidx.compose.ui:ui-tooling-preview:$composeUiVersion")
     implementation("androidx.compose.material3:material3:1.2.1")
 
     //Test

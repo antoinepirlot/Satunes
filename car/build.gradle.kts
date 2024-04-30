c47e8c13fedaba8a0283827dcc9036555585f92a
Antoine <pirlotantoine2@gmail.com>
2024-04-30 13:30:47 +0200

    Update: make the app compatible with Android Lollipop

--- car/build.gradle.kts
+++ car/build.gradle.kts
@@ -33,7 +33,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 23
+        minSdk = 22
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- database/build.gradle.kts
+++ database/build.gradle.kts
@@ -40,7 +40,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 23
+        minSdk = 22
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")

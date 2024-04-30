f5dead8ee76907f0becf210da2a2aaae61f9bb3a
Antoine <pirlotantoine2@gmail.com>
2024-04-30 11:06:20 +0200

    Update: make the app compatible with Android Nougat 7.1.1 (API 25)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,7 @@ android {
 
     defaultConfig {
         applicationId = nameSpace
-        minSdk = 26
+        minSdk = 25
         targetSdk = 34
         versionCode = 22
         versionName = "0.9.2-beta"

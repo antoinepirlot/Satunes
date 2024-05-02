5fe3756c88d6f3ac4c212b9ee6b3b24b6f90d846
Antoine <pirlotantoine2@gmail.com>
2024-05-02 20:39:49 +0200

    Fix: refresh bar progression is made only when needed (while foreground and on playback screen when playing)

--- app/src/main/java/io/github/antoinepirlot/satunes/ui/components/bars/MusicPositionBar.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/components/bars/MusicPositionBar.kt
@@ -1,26 +1,26 @@
 /*
  * This file is part of Satunes.
  *
- * Satunes is free software: you can redistribute it and/or modify it under
+ *  Satunes is free software: you can redistribute it and/or modify it under
  *  the terms of the GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version.
+ *  either version 3 of the License, or (at your option) any later version.
  *
- * Satunes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
+ *  Satunes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License along with Satunes.
- * If not, see <https://www.gnu.org/licenses/>.
+ *  You should have received a copy of the GNU General Public License along with Satunes.
+ *  If not, see <https://www.gnu.org/licenses/>.
  *
- * **** INFORMATIONS ABOUT THE AUTHOR *****
- * The author of this file is Antoine Pirlot, the owner of this project.
- * You find this original project on github.
+ *  **** INFORMATIONS ABOUT THE AUTHOR *****
+ *  The author of this file is Antoine Pirlot, the owner of this project.
+ *  You find this original project on github.
  *
- * My github link is: https://github.com/antoinepirlot
- * This current project's link is: https://github.com/antoinepirlot/MP3-Player
+ *  My github link is: https://github.com/antoinepirlot
+ *  This current project's link is: https://github.com/antoinepirlot/Satunes
  *
- * You can contact me via my email: pirlot.antoine@outlook.com
- * PS: I don't answer quickly.
+ *  You can contact me via my email: pirlot.antoine@outlook.com
+ *  PS: I don't answer quickly.
  */
 
 package io.github.antoinepirlot.satunes.ui.components.bars
@@ -32,7 +32,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
@@ -42,15 +41,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.lifecycleScope
-import io.github.antoinepirlot.satunes.database.services.settings.SettingsManager
 import io.github.antoinepirlot.satunes.playback.services.PlaybackController
 import io.github.antoinepirlot.satunes.services.ProgressBarLifecycleCallbacks
 import io.github.antoinepirlot.satunes.ui.utils.getMillisToTimeText
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 /**
  * @author Antoine Pirlot on 23/02/24
@@ -95,8 +88,9 @@ fun MusicPositionBar(
     }
 
     val isPlaying: Boolean by rememberSaveable { playbackController.isPlaying }
+    LocalLifecycleOwner.current.lifecycle.addObserver(ProgressBarLifecycleCallbacks)
     if (isPlaying) {
-        LocalLifecycleOwner.current.lifecycle.addObserver(ProgressBarLifecycleCallbacks)
+        ProgressBarLifecycleCallbacks.startUpdatingCurrentPosition()
     }
 }
 

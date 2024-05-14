a576f1499bc678f93a4b6849a1118bc7c24a1bb1
Antoine <pirlotantoine2@gmail.com>
2024-05-14 18:38:04 +0200

    Update: make image with link responsive

--- app/src/main/java/io/github/antoinepirlot/satunes/ui/components/images/ImageWithLink.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/components/images/ImageWithLink.kt
@@ -40,12 +40,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import io.github.antoinepirlot.satunes.R
+import io.github.antoinepirlot.satunes.ui.ScreenSizes
 import io.github.antoinepirlot.satunes.ui.components.settings.utils.openUrl
 
 /**
@@ -62,10 +65,15 @@ fun ImageWithLink(
 ) {
     val haptics = LocalHapticFeedback.current
     val context: Context = LocalContext.current
+    val screenWidthDp: Int = LocalConfiguration.current.screenWidthDp
     Box(modifier = modifier) {
+        val imageSize: Dp = if (screenWidthDp <= ScreenSizes.VERY_SMALL)
+            40.dp
+        else
+            60.dp
         Image(
             modifier = Modifier
-                .size(60.dp)
+                .size(imageSize)
                 .clip(CircleShape)
                 .align(Alignment.Center)
                 .background(imageBackgroundColor ?: Color.Unspecified)

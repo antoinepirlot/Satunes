06d7db597662610aa4db04b2396fceabba2b5228
Antoine <pirlotantoine2@gmail.com>
2024-05-28 16:06:18 +0200

    Update: show pop up when setting needs restart instead of simple text next to setting name

--- app/src/main/java/io/github/antoinepirlot/satunes/ui/components/settings/SettingsSwitchList.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/components/settings/SettingsSwitchList.kt
@@ -27,15 +27,25 @@ package io.github.antoinepirlot.satunes.ui.components.settings
 
 import android.content.Context
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Icon
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import io.github.antoinepirlot.satunes.R
 import io.github.antoinepirlot.satunes.database.models.MenuTitle
 import io.github.antoinepirlot.satunes.database.services.settings.SettingsManager
+import io.github.antoinepirlot.satunes.icons.SatunesIcons
 import io.github.antoinepirlot.satunes.ui.components.cards.ListItem
+import io.github.antoinepirlot.satunes.ui.components.texts.NormalText
 import io.github.antoinepirlot.satunes.ui.views.settings.Settings
 
 /**
@@ -48,25 +58,65 @@ fun SettingsSwitchList(
     checkedMap: Map<Settings, MutableState<Boolean>>,
 ) {
     val context: Context = LocalContext.current
+
+    //Used to show modal for restart needed setting
+    var clickedSetting: Settings? by rememberSaveable { mutableStateOf(null) }
+
     Column(
         modifier = modifier
     ) {
         for (setting: Settings in checkedMap.keys.toList()) {
+            val isRestartNeeded: Boolean = when (setting) {
+                Settings.EXCLUDE_RINGTONES, Settings.PAUSE_IF_NOISY,
+                Settings.PAUSE_IF_ANOTHER_PLAYBACK -> true
+
+                else -> false
+            }
             ListItem(
                 headlineContent = {
                     SettingWithSwitch(
                         text = stringResource(id = setting.stringId),
                         checked = checkedMap[setting]!!.value,
+                        icon = if (isRestartNeeded) SatunesIcons.INFO else null,
                         onCheckedChange = {
-                            switchSetting(
-                                context = context,
-                                setting = setting
-                            )
+                            if (isRestartNeeded) {
+                                clickedSetting = setting
+                            } else {
+                                switchSetting(
+                                    context = context,
+                                    setting = setting
+                                )
+                            }
                         }
                     )
                 }
             )
         }
+    }
+
+    //TODO use info composable in v1.1.0
+    if (clickedSetting != null) {
+        AlertDialog(
+            icon = {
+                val icon: SatunesIcons = SatunesIcons.INFO
+                Icon(imageVector = icon.imageVector, contentDescription = icon.description)
+            },
+            title = {
+                NormalText(text = stringResource(id = R.string.restart_required))
+            },
+            onDismissRequest = { clickedSetting = null },
+            confirmButton = {
+                TextButton(onClick = {
+                    switchSetting(
+                        context = context,
+                        setting = clickedSetting!!
+                    )
+                    clickedSetting = null
+                }) {
+                    NormalText(text = stringResource(id = R.string.ok))
+                }
+            }
+        )
     }
 }
 

de8c65f879dec5fdd8b2c1633300c75665db9a4b
Antoine <pirlotantoine2@gmail.com>
2024-06-25 15:51:23 +0200

    Fix: close modal when add to queue finished

--- app/src/main/java/io/github/antoinepirlot/satunes/ui/components/dialog/media/options/AddToQueueMediaOption.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/components/dialog/media/options/AddToQueueMediaOption.kt
@@ -51,6 +51,7 @@ internal fun AddToQueueDialogOption(
         modifier = modifier,
         onClick = {
             PlaybackController.getInstance().addToQueue(media = media)
+            onFinished()
         },
         icon = {
             val icon: SatunesIcons = SatunesIcons.ADD_TO_PLAYBACK_QUEUE

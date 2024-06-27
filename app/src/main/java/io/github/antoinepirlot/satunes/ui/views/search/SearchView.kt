d85a04d331f25b1a670ec208360f451746195597
Antoine <pirlotantoine2@gmail.com>
2024-06-28 00:32:16 +0200

    Add: search bar

--- app/src/main/java/io/github/antoinepirlot/satunes/ui/views/search/SearchView.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/views/search/SearchView.kt
@@ -25,20 +25,90 @@
 
 package io.github.antoinepirlot.satunes.ui.views.search
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SearchBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import io.github.antoinepirlot.satunes.R
+import io.github.antoinepirlot.satunes.database.models.Music
+import io.github.antoinepirlot.satunes.database.services.DataManager
+import io.github.antoinepirlot.satunes.playback.services.PlaybackController
+import io.github.antoinepirlot.satunes.router.utils.openMedia
+import io.github.antoinepirlot.satunes.ui.components.cards.media.MediaCardList
+import io.github.antoinepirlot.satunes.ui.components.texts.NormalText
 
 /**
  * @author Antoine Pirlot on 27/06/2024
  */
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 internal fun SearchView(
     modifier: Modifier = Modifier
 ) {
-    Text(text = "Hello World !")
+    var query: String by rememberSaveable { mutableStateOf("") }
+    var isSearchBarActive: Boolean by rememberSaveable { mutableStateOf(false) }
+    val musicList: MutableList<Music> = remember { SnapshotStateList() }
+
+    LaunchedEffect(key1 = true) {
+        musicList.addAll(DataManager.musicMediaItemSortedMap.keys)
+    }
+
+    Column(modifier = modifier.fillMaxSize()) {
+        SearchBar(
+            query = query,
+            onQueryChange = {
+                query = it
+                musicList.clear()
+                musicList.addAll(
+                    DataManager.musicMediaItemSortedMap.keys.filter { music: Music ->
+                        music.title.lowercase().contains(query.lowercase())
+                    }
+                )
+            },
+            onSearch = {
+                query = it
+                isSearchBarActive = false
+            },
+            active = isSearchBarActive,
+            onActiveChange = { isSearchBarActive = it },
+            placeholder = { NormalText(text = stringResource(id = R.string.search_placeholder)) }
+        ) {
+            if (musicList.isEmpty()) {
+                NormalText(text = stringResource(id = R.string.no_result))
+            } else {
+                MediaCardList(mediaList = musicList, openMedia = {
+                    PlaybackController.getInstance()
+                        .loadMusic(musicMediaItemSortedMap = DataManager.musicMediaItemSortedMap)
+                    openMedia(media = it)
+                })
+            }
+        }
+
+        if (!isSearchBarActive) {
+            // Also show result when user leave search bar focus
+            if (musicList.isEmpty()) {
+                NormalText(text = stringResource(id = R.string.no_result))
+            } else {
+                MediaCardList(mediaList = musicList, openMedia = {
+                    PlaybackController.getInstance()
+                        .loadMusic(musicMediaItemSortedMap = DataManager.musicMediaItemSortedMap)
+                    openMedia(media = it)
+                })
+            }
+        }
+    }
 }
 
 @Preview

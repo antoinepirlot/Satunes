373d2bbe4aed6c0d6b05b4a5805638611b68c601
Antoine <pirlotantoine2@gmail.com>
2024-06-30 13:41:26 +0200

    Fix: be sure the music to add next is not the music playing

--- playback/src/main/java/io/github/antoinepirlot/satunes/playback/services/PlaybackController.kt
+++ playback/src/main/java/io/github/antoinepirlot/satunes/playback/services/PlaybackController.kt
@@ -337,6 +337,9 @@ class PlaybackController private constructor(
     }
 
     fun addNext(media: Media) {
+        if (musicPlaying.value == media) {
+            return
+        }
         when (media) {
             is Music -> {
                 try {

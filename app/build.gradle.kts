940db3fff367a3eaa2a35a515f98b793c7275020
Antoine <pirlotantoine2@gmail.com>
2024-06-30 15:07:09 +0200

    Update: upgrade version code to 35 and version name to "v1.2.0"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = nameSpace
         minSdk = 22
         targetSdk = 34
-        versionCode = 34
-        versionName = "1.2.0-preview-1"
+        versionCode = 35
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {

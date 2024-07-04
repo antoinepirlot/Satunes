2e8c035ac5d276a336e18c05d3f1964cbc06fe51
Antoine <pirlotantoine2@gmail.com>
2024-07-04 23:42:42 +0200

    Update: show the keyboard when opening search view

--- app/src/main/java/io/github/antoinepirlot/satunes/ui/views/search/SearchView.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/views/search/SearchView.kt
@@ -42,6 +42,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -89,11 +91,18 @@ internal fun SearchView(
         }
     }
 
+    val focusRequester: FocusRequester = remember { FocusRequester() }
+    LaunchedEffect(key1 = true) {
+        // Request focus after composable becomes visible
+        focusRequester.requestFocus()
+    }
+
     Column(
         modifier = modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         SearchBar(
+            modifier = Modifier.focusRequester(focusRequester),
             query = query,
             onQueryChange = { query = it },
             onSearch = { query = it },

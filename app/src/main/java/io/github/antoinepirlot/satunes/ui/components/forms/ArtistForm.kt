f44788f379995f9bb3bab6e4848b4be93e4652d9
Antoine <pirlotantoine2@gmail.com>
2024-07-06 12:59:08 +0200

    Add: Artist form with title text field

--- app/src/main/java/io/github/antoinepirlot/satunes/ui/components/forms/ArtistForm.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/components/forms/ArtistForm.kt
@@ -23,66 +23,51 @@
  *  PS: I don't answer quickly.
  */
 
-package io.github.antoinepirlot.satunes.ui.views.media.forms
+package io.github.antoinepirlot.satunes.ui.components.forms
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import io.github.antoinepirlot.satunes.database.models.Album
+import io.github.antoinepirlot.satunes.R
 import io.github.antoinepirlot.satunes.database.models.Artist
-import io.github.antoinepirlot.satunes.database.models.Folder
-import io.github.antoinepirlot.satunes.database.models.Genre
-import io.github.antoinepirlot.satunes.database.models.Music
-import io.github.antoinepirlot.satunes.ui.components.forms.ArtistForm
-import io.github.antoinepirlot.satunes.ui.components.forms.MusicForm
+import io.github.antoinepirlot.satunes.ui.components.texts.NormalText
+import io.github.antoinepirlot.satunes.ui.components.texts.Title
 
 /**
  * @author Antoine Pirlot on 06/07/2024
  */
 
 @Composable
-internal fun MusicFormView(
+internal fun ArtistForm(
     modifier: Modifier = Modifier,
-    music: Music,
+    artist: Artist,
 ) {
     Column(
         modifier = modifier,
     ) {
-        MusicForm(music = music)
+        Title(text = stringResource(id = R.string.artistFormTitle))
 
-        ArtistForm(artist = music.artist)
-
-        /**
-         * Album
-         */
-
-        /**
-         * Genre
-         */
+        var artistTitle: String by rememberSaveable { mutableStateOf(artist.title) }
+        OutlinedTextField(
+            value = artistTitle,
+            onValueChange = { artistTitle = it },
+            label = {
+                NormalText(text = stringResource(id = R.string.title))
+            }
+        )
     }
 }
 
 @Preview
 @Composable
-private fun MusicFormViewPreview() {
-    val album = Album(title = "Album title")
+private fun ArtistFormPreview() {
     val artist = Artist(title = "Artist title")
-    val genre = Genre(title = "Genre title")
-    val folder = Folder(title = "Folder title")
-    val music = Music(
-        id = 0,
-        title = "Music title",
-        album = album,
-        artist = artist,
-        genre = genre,
-        absolutePath = "",
-        context = LocalContext.current,
-        folder = folder,
-        size = 0,
-        duration = 0,
-        displayName = "Display Name"
-    )
-    MusicFormView(music = music)
+    ArtistForm(artist = artist)
 }

bf84167458c61a831d8bc813126bd78fa2bcb6db
Antoine <pirlotantoine2@gmail.com>
2024-07-06 13:17:24 +0200

    Add: Genre form with title text field

--- app/src/main/java/io/github/antoinepirlot/satunes/ui/components/forms/GenreForm.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/components/forms/GenreForm.kt
@@ -1,86 +1,73 @@
 /*
  * This file is part of Satunes.
- *
+ *  
  *  Satunes is free software: you can redistribute it and/or modify it under
  *  the terms of the GNU General Public License as published by the Free Software Foundation,
  *  either version 3 of the License, or (at your option) any later version.
- *
+ *  
  *  Satunes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *  See the GNU General Public License for more details.
- *
+ *  
  *  You should have received a copy of the GNU General Public License along with Satunes.
  *  If not, see <https://www.gnu.org/licenses/>.
- *
+ *  
  *  **** INFORMATIONS ABOUT THE AUTHOR *****
  *  The author of this file is Antoine Pirlot, the owner of this project.
  *  You find this original project on github.
- *
+ *  
  *  My github link is: https://github.com/antoinepirlot
  *  This current project's link is: https://github.com/antoinepirlot/Satunes
- *
+ *  
  *  You can contact me via my email: pirlot.antoine@outlook.com
  *  PS: I don't answer quickly.
  */
 
-package io.github.antoinepirlot.satunes.ui.views.media.forms
+package io.github.antoinepirlot.satunes.ui.components.forms
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import io.github.antoinepirlot.satunes.database.models.Album
-import io.github.antoinepirlot.satunes.database.models.Artist
-import io.github.antoinepirlot.satunes.database.models.Folder
+import io.github.antoinepirlot.satunes.R
 import io.github.antoinepirlot.satunes.database.models.Genre
-import io.github.antoinepirlot.satunes.database.models.Music
-import io.github.antoinepirlot.satunes.ui.components.forms.AlbumForm
-import io.github.antoinepirlot.satunes.ui.components.forms.ArtistForm
-import io.github.antoinepirlot.satunes.ui.components.forms.GenreForm
-import io.github.antoinepirlot.satunes.ui.components.forms.MusicForm
+import io.github.antoinepirlot.satunes.ui.components.texts.NormalText
+import io.github.antoinepirlot.satunes.ui.components.texts.Title
 
 /**
  * @author Antoine Pirlot on 06/07/2024
  */
 
 @Composable
-internal fun MusicFormView(
+internal fun GenreForm(
     modifier: Modifier = Modifier,
-    music: Music,
+    genre: Genre
 ) {
     Column(
         modifier = modifier,
     ) {
-        MusicForm(music = music)
+        Title(text = stringResource(id = R.string.genre))
 
-        ArtistForm(artist = music.artist)
-
-        AlbumForm(album = music.album)
-
-        GenreForm(genre = music.genre)
+        var genreTitle: String by rememberSaveable { mutableStateOf(genre.title) }
+        OutlinedTextField(
+            value = genreTitle,
+            onValueChange = { genreTitle = it },
+            label = {
+                NormalText(text = stringResource(id = R.string.title))
+            }
+        )
     }
 }
 
 @Preview
 @Composable
-private fun MusicFormViewPreview() {
-    val album = Album(title = "Album title")
-    val artist = Artist(title = "Artist title")
+private fun GenreFormPreview() {
     val genre = Genre(title = "Genre title")
-    val folder = Folder(title = "Folder title")
-    val music = Music(
-        id = 0,
-        title = "Music title",
-        album = album,
-        artist = artist,
-        genre = genre,
-        absolutePath = "",
-        context = LocalContext.current,
-        folder = folder,
-        size = 0,
-        duration = 0,
-        displayName = "Display Name"
-    )
-    MusicFormView(music = music)
+    GenreForm(genre = genre)
 }

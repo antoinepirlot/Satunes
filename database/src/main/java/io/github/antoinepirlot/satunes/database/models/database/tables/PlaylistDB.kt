88f4046cc5c42e87fb4ee48ecc3de55f1e962406
Antoine <pirlotantoine2@gmail.com>
2024-07-09 23:27:16 +0200

    Update: playlist is identified by its lowercase title

--- database/src/main/java/io/github/antoinepirlot/satunes/database/models/database/tables/PlaylistDB.kt
+++ database/src/main/java/io/github/antoinepirlot/satunes/database/models/database/tables/PlaylistDB.kt
@@ -68,4 +68,16 @@ data class PlaylistDB(
     @Ignore
     @Transient
     override var artwork: Bitmap? = null
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as PlaylistDB
+
+        return title.lowercase() == other.title.lowercase()
+    }
+
+    override fun hashCode(): Int {
+        return title.lowercase().hashCode()
+    }
 }

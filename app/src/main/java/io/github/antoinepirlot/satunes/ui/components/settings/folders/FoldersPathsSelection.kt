6c5256797d1b14894e8f7cb90904dae6206e98a7
Antoine <pirlot.antoine@outlook.com>
2024-08-09 15:26:09 +0200

    Add: Button to add path
    Signed-off-by: Antoine <pirlot.antoine@outlook.com>

--- app/src/main/java/io/github/antoinepirlot/satunes/ui/components/settings/folders/FoldersPathsSelection.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/components/settings/folders/FoldersPathsSelection.kt
@@ -25,17 +25,26 @@
 
 package io.github.antoinepirlot.satunes.ui.components.settings.folders
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import io.github.antoinepirlot.jetpack_libs.components.texts.NormalText
+import io.github.antoinepirlot.satunes.R
 import io.github.antoinepirlot.satunes.data.states.SatunesUiState
 import io.github.antoinepirlot.satunes.data.viewmodels.SatunesViewModel
+import io.github.antoinepirlot.satunes.icons.SatunesIcons
+import io.github.antoinepirlot.satunes.ui.components.buttons.ButtonWithIcon
 
 /**
  * @author Antoine Pirlot on 09/08/2024
@@ -45,18 +54,36 @@ import io.github.antoinepirlot.satunes.data.viewmodels.SatunesViewModel
 internal fun FoldersPathsSelection(
     modifier: Modifier = Modifier,
     satunesViewModel: SatunesViewModel = viewModel(),
+
 ) {
     val satunesUiState: SatunesUiState by satunesViewModel.uiState.collectAsState()
 
-    LazyColumn(
-        modifier = modifier
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        items(
-            items = satunesUiState.foldersPathsSelectedList.toList(),
-            key = { it },
-        ) {
-            NormalText(text = it.removeSuffix("%"))
+        LazyColumn {
+            items(
+                items = satunesUiState.foldersPathsSelectedList.toList(),
+                key = { it },
+            ) {
+                NormalText(text = it.removeSuffix("%"))
+            }
         }
+
+        Spacer(modifier = Modifier.size(5.dp))
+
+        //Show text "Add path to exclude/include".
+        ButtonWithIcon(
+            icon = SatunesIcons.ADD,
+            onClick = { /*TODO*/ },
+            text = stringResource(
+                id = R.string.add_path_button,
+                stringResource(
+                    id = satunesUiState.foldersSelectionSelected.stringId
+                )
+            )
+        )
     }
 }
 

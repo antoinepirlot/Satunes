cd8cf9fb585adf7a95fab1bcfa641fc135257e1d
Antoine <pirlot.antoine@outlook.com>
2024-09-12 15:33:41 +0200

    Refactor: move folders path row to its own composable
    Signed-off-by: Antoine <pirlot.antoine@outlook.com>

--- app/src/main/java/io/github/antoinepirlot/satunes/ui/components/buttons/settings/library/folders/FolderPathRow.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/components/buttons/settings/library/folders/FolderPathRow.kt
@@ -1,31 +1,32 @@
 /*
  * This file is part of Satunes.
- *  
+ *
  *  Satunes is free software: you can redistribute it and/or modify it under
  *  the terms of the GNU General Public License as published by the Free Software Foundation,
  *  either version 3 of the License, or (at your option) any later version.
- *  
+ *
  *  Satunes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  *  See the GNU General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License along with Satunes.
  *  If not, see <https://www.gnu.org/licenses/>.
- *  
+ *
  *  **** INFORMATIONS ABOUT THE AUTHOR *****
  *  The author of this file is Antoine Pirlot, the owner of this project.
  *  You find this original project on github.
- *  
+ *
  *  My github link is: https://github.com/antoinepirlot
  *  This current project's link is: https://github.com/antoinepirlot/Satunes
- *  
+ *
  *  You can contact me via my email: pirlot.antoine@outlook.com
  *  PS: I don't answer quickly.
  */
 
-package io.github.antoinepirlot.satunes.ui.components.settings.library.folders
+package io.github.antoinepirlot.satunes.ui.components.buttons.settings.library.folders
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
@@ -39,47 +40,54 @@ import io.github.antoinepirlot.satunes.R
 import io.github.antoinepirlot.satunes.data.local.LocalMainScope
 import io.github.antoinepirlot.satunes.data.local.LocalSnackBarHostState
 import io.github.antoinepirlot.satunes.data.viewmodels.SatunesViewModel
-import io.github.antoinepirlot.satunes.ui.components.buttons.settings.library.folders.FolderPathRow
-import io.github.antoinepirlot.satunes.ui.components.buttons.settings.library.folders.FoldersPathButtons
+import io.github.antoinepirlot.satunes.icons.SatunesIcons
+import io.github.antoinepirlot.satunes.ui.components.buttons.IconButton
 import kotlinx.coroutines.CoroutineScope
 
 /**
- * @author Antoine Pirlot on 09/08/2024
+ * @author Antoine Pirlot on 12/09/2024
  */
 
 @Composable
-internal fun FoldersPathsSelection(
+internal fun FolderPathRow(
     modifier: Modifier = Modifier,
     satunesViewModel: SatunesViewModel = viewModel(),
+    path: String,
 ) {
     val scope: CoroutineScope = LocalMainScope.current
     val snackBarHostState: SnackbarHostState = LocalSnackBarHostState.current
 
-    Column(
-        modifier = modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
     ) {
-        for (path: String in satunesViewModel.foldersPathsSelectedSet.toList()) {
-            FolderPathRow(path = path)
-
-            if (path == satunesViewModel.foldersPathsSelectedSet.last()) {
-                FoldersPathButtons()
+        NormalText(
+            modifier = Modifier.fillMaxWidth(fraction = 0.8f),
+            text = if (path.startsWith("/0/")) {
+                path.replaceFirst(
+                    "/0/",
+                    stringResource(id = R.string.this_device) + '/'
+                )
+            } else {
+                path
+            }.removeSuffix("%")
+        )
+        IconButton(
+            icon = SatunesIcons.REMOVE_ICON,
+            onClick = {
+                satunesViewModel.removePath(
+                    scope = scope,
+                    snackBarHostState = snackBarHostState,
+                    path = path
+                )
             }
-        }
-    }
-    if (satunesViewModel.foldersPathsSelectedSet.isEmpty()) {
-        Column(
-            modifier = modifier.fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            NormalText(text = stringResource(id = R.string.path_set_empty))
-            FoldersPathButtons()
-        }
+        )
     }
 }
 
 @Preview
 @Composable
-private fun FoldersPathsSelectionPreview() {
-    FoldersPathsSelection()
+private fun FolderPathRowPreview() {
+    FoldersPathButtons()
 }

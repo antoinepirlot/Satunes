828da05a77bd03a811b8ec53e36a49c38f145d5d
Antoine <pirlot.antoine@outlook.com>
2024-09-23 22:17:36 +0200

    Remove: dialog when clicking on reload library
    Signed-off-by: Antoine <pirlot.antoine@outlook.com>

--- app/src/main/java/io/github/antoinepirlot/satunes/ui/components/buttons/settings/library/folders/FoldersPathsButtons.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/components/buttons/settings/library/folders/FoldersPathsButtons.kt
@@ -27,19 +27,13 @@ package io.github.antoinepirlot.satunes.ui.components.buttons.settings.library.f
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -47,18 +41,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import io.github.antoinepirlot.jetpack_libs.components.texts.NormalText
 import io.github.antoinepirlot.satunes.R
-import io.github.antoinepirlot.satunes.data.local.LocalMainScope
-import io.github.antoinepirlot.satunes.data.local.LocalSnackBarHostState
 import io.github.antoinepirlot.satunes.data.states.SatunesUiState
 import io.github.antoinepirlot.satunes.data.viewmodels.DataViewModel
 import io.github.antoinepirlot.satunes.data.viewmodels.PlaybackViewModel
 import io.github.antoinepirlot.satunes.data.viewmodels.SatunesViewModel
 import io.github.antoinepirlot.satunes.icons.SatunesIcons
 import io.github.antoinepirlot.satunes.ui.components.buttons.ButtonWithIcon
-import io.github.antoinepirlot.satunes.ui.components.dialog.WarningDialog
-import kotlinx.coroutines.CoroutineScope
 
 /**
  * @author Antoine Pirlot on 12/09/2024
@@ -72,12 +61,8 @@ internal fun FoldersPathButtons(
     dataViewModel: DataViewModel = viewModel(),
 ) {
     val satunesUiState: SatunesUiState by satunesViewModel.uiState.collectAsState()
-    val scope: CoroutineScope = LocalMainScope.current
-    val snackBarHostState: SnackbarHostState = LocalSnackBarHostState.current
 
     val spacerSize: Dp = 5.dp
-
-    var showDialog: Boolean by rememberSaveable { mutableStateOf(false) }
 
     //Show text "Add path to exclude/include".
     Column(
@@ -102,38 +87,11 @@ internal fun FoldersPathButtons(
         ButtonWithIcon(
             modifier = Modifier.fillMaxWidth(),
             icon = SatunesIcons.REFRESH,
-            onClick = { showDialog = true },
+            onClick = { satunesViewModel.reloadAllData(playbackViewModel = playbackViewModel) },
             enabled = !satunesViewModel.isLoadingData,
             isLoading = satunesViewModel.isLoadingData,
             text = stringResource(id = R.string.refresh_data_button)
         )
-        if (showDialog) {
-            WarningDialog(
-                text = stringResource(id = R.string.export_all_dialog_content),
-                onDismissRequest = { showDialog = false },
-                confirmButton = {
-                    Row {
-                        TextButton(onClick = {
-                            showDialog = false
-                            satunesViewModel.reloadAllData(playbackViewModel = playbackViewModel)
-                        }) {
-                            NormalText(text = stringResource(id = R.string.reload_library_no_export_button))
-                        }
-
-                        TextButton(onClick = {
-                            dataViewModel.exportPlaylists(
-                                scope = scope,
-                                snackBarHostState = snackBarHostState
-                            )
-                            showDialog = false
-                            satunesViewModel.reloadAllData(playbackViewModel = playbackViewModel)
-                        }) {
-                            NormalText(text = stringResource(id = R.string.export))
-                        }
-                    }
-                }
-            )
-        }
     }
 }
 

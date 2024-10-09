e8e214be3750c05ae3856b0c5421c9504bea75d1
Antoine Pirlot <pirlot.antoine@outlook.com>
2024-10-09 08:54:13 +0200

    Add: timer selection zone
    Signed-off-by: Antoine Pirlot <pirlot.antoine@outlook.com>

---
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/components/settings/playback/TimerSubSettings.kt
@@ -0,0 +1,97 @@
+/*
+ * This file is part of Satunes.
+ *
+ *  Satunes is free software: you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free Software Foundation,
+ *  either version 3 of the License, or (at your option) any later version.
+ *
+ *  Satunes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *  See the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along with Satunes.
+ *  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *  **** INFORMATIONS ABOUT THE AUTHOR *****
+ *  The author of this file is Antoine Pirlot, the owner of this project.
+ *  You find this original project on github.
+ *
+ *  My github link is: https://github.com/antoinepirlot
+ *  This current project's link is: https://github.com/antoinepirlot/Satunes
+ *
+ *  You can contact me via my email: pirlot.antoine@outlook.com
+ *  PS: I don't answer quickly.
+ */
+
+package io.github.antoinepirlot.satunes.ui.components.settings.playback
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableIntState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.text.isDigitsOnly
+import io.github.antoinepirlot.jetpack_libs.components.texts.NormalText
+import io.github.antoinepirlot.satunes.R
+import io.github.antoinepirlot.satunes.ui.components.settings.SubSettings
+
+/**
+ * @author Antoine Pirlot on 03/10/2024
+ */
+
+@Composable
+internal fun TimerSubSetting(modifier: Modifier = Modifier) {
+    SubSettings(
+        modifier = modifier,
+        title = stringResource(R.string.timer_settings_title)
+    ) {
+        Column(modifier = Modifier.selectableGroup()) {
+            Row {
+                var minutesTextField: MutableIntState = rememberSaveable { mutableIntStateOf(0) }
+                OutlinedNumberField(
+                    value = minutesTextField,
+                    label = stringResource(R.string.minutes_text_field_label),
+                    maxValue = 480
+                ) //max 8 hours
+            }
+        }
+    }
+}
+
+@Composable
+private fun OutlinedNumberField(
+    modifier: Modifier = Modifier,
+    value: MutableIntState,
+    label: String,
+    maxValue: Int
+) {
+    val keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+    OutlinedTextField(
+        modifier = modifier,
+        value = value.intValue.toString(),
+        label = { NormalText(text = label) },
+        onValueChange = {
+            if (it.isNotBlank() && it.isDigitsOnly()) {
+                val itAsInt: Int = it.toInt()
+                if (itAsInt <= maxValue)
+                    value.intValue = it.toInt()
+            }
+        },
+        keyboardOptions = keyboardOptions,
+    )
+}
+
+
+@Preview
+@Composable
+private fun TimerSubSettingPreview() {
+    TimerSubSetting()
+}

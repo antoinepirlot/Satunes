0ffba549ad98c9c3e193b00665fe9843b5db6835
Antoine <pirlot.antoine@outlook.com>
2024-11-05 17:04:04 +0100

    Remove: exclusion of some FOSS libraries
    Signed-off-by: Antoine <pirlot.antoine@outlook.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,11 +61,6 @@ android {
     kotlinOptions {
         jvmTarget = "17"
     }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
 
     dependenciesInfo {
         // Disables dependency metadata when building APKs.

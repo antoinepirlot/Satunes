11b41aca90fa4cb050a5e4e63c214bbf770a1ab6
Antoine <pirlot.antoine@outlook.com>
2024-12-22 15:44:57 +0100

    Update: implement sort by genre
    Signed-off-by: Antoine <pirlot.antoine@outlook.com>

--- database/src/main/java/io/github/antoinepirlot/satunes/database/models/comparators/SortByGenreComparator.kt
+++ database/src/main/java/io/github/antoinepirlot/satunes/database/models/comparators/SortByGenreComparator.kt
@@ -25,13 +25,34 @@
 
 package io.github.antoinepirlot.satunes.database.models.comparators
 
+import com.mpatric.mp3agic.NotSupportedException
+import io.github.antoinepirlot.satunes.database.models.Genre
 import io.github.antoinepirlot.satunes.database.models.MediaImpl
+import io.github.antoinepirlot.satunes.database.models.Music
 
 /**
- * @author Antoine Pirlot on 22/12/2024
+ * Compare [MediaImpl] by [Genre].
+ * If it is the same [Genre] then compare by [MediaImpl]'s title.
+ *
+ * @throws NotSupportedException if both element doesn't have a genre.
+ *
+ *  @author Antoine Pirlot on 22/12/2024
  */
 object SortByGenreComparator : Comparator<MediaImpl> {
-    override fun compare(o1: MediaImpl?, o2: MediaImpl?): Int {
-        TODO("Not yet implemented")
+    override fun compare(mediaImpl1: MediaImpl, mediaImpl2: MediaImpl): Int {
+        return when (mediaImpl1) {
+            is Music -> {
+                when (mediaImpl2) {
+                    is Music -> mediaImpl1.genre.compareTo(mediaImpl2.genre)
+                    else -> 1 // mediaImpl2 is not a music, so the mediaImpl2 goes to the end
+                }
+            }
+
+            else ->
+                when (mediaImpl2) {
+                    is Music -> -1
+                    else -> throw NotSupportedException("Can't sort ${mediaImpl1.javaClass.name} by genre.")
+                }
+        }
     }
 }

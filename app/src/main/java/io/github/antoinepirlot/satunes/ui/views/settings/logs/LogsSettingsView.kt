79fdb4729fff919a42e419309c72e1b194d97152
Antoine Pirlot <antoinepirlot@proton.me>
2025-04-19 20:36:16 +0200

    Add: space between logs text and buttons
    Signed-off-by: Antoine Pirlot <antoinepirlot@proton.me>

--- app/src/main/java/io/github/antoinepirlot/satunes/ui/views/settings/logs/LogsSettingsView.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/views/settings/logs/LogsSettingsView.kt
@@ -22,6 +22,8 @@ package io.github.antoinepirlot.satunes.ui.views.settings.logs
 
 import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -33,6 +35,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import io.github.antoinepirlot.jetpack_libs.components.texts.NormalText
 import io.github.antoinepirlot.jetpack_libs.components.texts.Title
@@ -69,6 +72,8 @@ internal fun LogsSettingsView(
             text = stringResource(id = R.string.logs_settings_content),
             maxLines = Int.MAX_VALUE
         )
+
+        Spacer(modifier = Modifier.size(size = 50.dp))
 
         SettingWithSwitch(
             setting = SwitchSettings.LOGS,

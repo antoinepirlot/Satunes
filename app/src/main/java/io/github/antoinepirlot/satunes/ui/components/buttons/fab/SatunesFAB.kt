aa1cddeb773d3468ab3a08a9a8ff830dccc39161
Antoine Pirlot <antoinepirlot@proton.me>
2025-04-20 10:55:51 +0200

    Fix: do not show extra button list when data is reloading
    Signed-off-by: Antoine Pirlot <antoinepirlot@proton.me>

--- app/src/main/java/io/github/antoinepirlot/satunes/ui/components/buttons/fab/SatunesFAB.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/components/buttons/fab/SatunesFAB.kt
@@ -1,16 +1,15 @@
 /*
  * This file is part of Satunes.
- *
  * Satunes is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
- * Satunes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ *  Satunes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License along with Satunes.
- * If not, see <https://www.gnu.org/licenses/>.
+ *  You should have received a copy of the GNU General Public License along with Satunes.
+ *  If not, see <https://www.gnu.org/licenses/>.
  *
- * *** INFORMATION ABOUT THE AUTHOR *****
+ * **** INFORMATION ABOUT THE AUTHOR *****
  * The author of this file is Antoine Pirlot, the owner of this project.
  * You find this original project on Codeberg.
  *
@@ -36,6 +35,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import io.github.antoinepirlot.satunes.data.local.LocalSnackBarHostState
 import io.github.antoinepirlot.satunes.data.states.SatunesUiState
+import io.github.antoinepirlot.satunes.data.viewmodels.DataViewModel
 import io.github.antoinepirlot.satunes.data.viewmodels.PlaybackViewModel
 import io.github.antoinepirlot.satunes.data.viewmodels.SatunesViewModel
 import io.github.antoinepirlot.satunes.models.DestinationCategory
@@ -49,6 +49,7 @@ internal fun SatunesFAB(
     modifier: Modifier = Modifier,
     satunesViewModel: SatunesViewModel = viewModel(),
     playbackViewModel: PlaybackViewModel = viewModel(),
+    dataViewModel: DataViewModel = viewModel()
 ) {
     val satunesUiState: SatunesUiState by satunesViewModel.uiState.collectAsState()
     val snackBarHostState: SnackbarHostState = LocalSnackBarHostState.current
@@ -60,6 +61,8 @@ internal fun SatunesFAB(
         Column(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            if (!dataViewModel.isLoaded) return
+
             val isInMediaListViews: Boolean =
                 satunesUiState.currentDestination.category == DestinationCategory.MEDIA
             if (isInMediaListViews) satunesUiState.extraButtons?.invoke()

0dc874350f1d76c511529ea3d3d165d2bd883241
Antoine Pirlot <antoinepirlot@proton.me>
2025-05-25 12:50:24 +0200

    Update: use the same sort options as musics
    Signed-off-by: Antoine Pirlot <antoinepirlot@proton.me>

--- app/src/main/java/io/github/antoinepirlot/satunes/data/SortOptions.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/data/SortOptions.kt
@@ -78,6 +78,26 @@
  * This current project's link is: https://codeberg.org/antoinepirlot/Satunes
  */
 
+/*
+ * This file is part of Satunes.
+ *
+ * Satunes is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ * Satunes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with Satunes.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ * *** INFORMATION ABOUT THE AUTHOR *****
+ * The author of this file is Antoine Pirlot, the owner of this project.
+ * You find this original project on Codeberg.
+ *
+ * My Codeberg link is: https://codeberg.org/antoinepirlot
+ * This current project's link is: https://codeberg.org/antoinepirlot/Satunes
+ */
+
 package io.github.antoinepirlot.satunes.data
 
 import io.github.antoinepirlot.satunes.models.Destination
@@ -121,14 +141,7 @@ private val playlistsSortOptions: List<SortOptions> = listOf(
     SortOptions.TITLE
 )
 
-private val singlePlaylistSortOptions: List<SortOptions> = listOf(
-    SortOptions.TITLE,
-    SortOptions.ALBUM,
-    SortOptions.ARTIST,
-    SortOptions.GENRE,
-    SortOptions.YEAR,
-    SortOptions.DATE_ADDED
-)
+private val singlePlaylistSortOptions: List<SortOptions> = musicsSortOptions
 
 /**
  * Get the sortOptions list for the specified [Destination].

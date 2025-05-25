52e6aa4a94ea3a8c55cde38482ac68a4ac7976d7
Antoine Pirlot <antoinepirlot@proton.me>
2025-05-25 13:03:33 +0200

    Update: make refresh widget available for all future widget
    Signed-off-by: Antoine Pirlot <antoinepirlot@proton.me>

--- app/src/main/java/io/github/antoinepirlot/satunes/widgets/PlaybackWidgetObject.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/widgets/PlaybackWidgetObject.kt
@@ -4,6 +4,26 @@
  * Satunes is free software: you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software Foundation,
  * either version 3 of the License, or (at your option) any later version.
+ * Satunes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with Satunes.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ * *** INFORMATION ABOUT THE AUTHOR *****
+ * The author of this file is Antoine Pirlot, the owner of this project.
+ * You find this original project on Codeberg.
+ *
+ * My Codeberg link is: https://codeberg.org/antoinepirlot
+ * This current project's link is: https://codeberg.org/antoinepirlot/Satunes
+ */
+
+/*
+ * This file is part of Satunes.
+ *
+ * Satunes is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
  *
  * Satunes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
@@ -37,22 +57,29 @@ import kotlinx.coroutines.launch
 object PlaybackWidget {
     private val _widgets: MutableMap<GlanceId, GlanceAppWidget> = mutableMapOf()
 
-    fun addWidget(glanceId: GlanceId, widget: GlanceAppWidget) {
+    fun addWidget(context: Context, glanceId: GlanceId, widget: GlanceAppWidget) {
         if (this._widgets.contains(key = glanceId)) return
         this._widgets[glanceId] = widget
+        setRefreshWidget(context = context)
+        WidgetPlaybackManager.refreshWidgets()
     }
 
     fun setRefreshWidget(context: Context) {
-        val refreshWidgets: () -> Unit = {
-            CoroutineScope(Dispatchers.Default).launch {
-                for (entry: Map.Entry<GlanceId, GlanceAppWidget> in _widgets.entries)
-                    entry.value.update(context = context.applicationContext, id = entry.key)
+        if (this._widgets.isEmpty())
+            WidgetPlaybackManager.setRefreshWidgets(refreshWidgets = null)
+        else {
+            val refreshWidgets: () -> Unit = {
+                CoroutineScope(Dispatchers.Default).launch {
+                    for (entry: Map.Entry<GlanceId, GlanceAppWidget> in _widgets.entries)
+                        entry.value.update(context = context.applicationContext, id = entry.key)
+                }
             }
+            WidgetPlaybackManager.setRefreshWidgets(refreshWidgets = refreshWidgets)
         }
-        WidgetPlaybackManager.setRefreshWidgets(refreshWidgets = refreshWidgets)
     }
 
-    fun removeWidget(glanceId: GlanceId) {
+    fun removeWidget(context: Context, glanceId: GlanceId) {
         this._widgets.remove(key = glanceId)
+        setRefreshWidget(context = context)
     }
 }

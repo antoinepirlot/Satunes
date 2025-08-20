1e71c903a11103eb787f5369d05821b2ff781840
Antoine Pirlot <antoinepirlot@proton.me>
2025-08-20 18:08:51 +0200

    Fix: do not go to the same route multiple times
    Signed-off-by: Antoine Pirlot <antoinepirlot@proton.me>

---
+++ app/src/main/java/io/github/antoinepirlot/satunes/models/listeners/OnDestinationChangedListener.kt
@@ -0,0 +1,42 @@
+/*
+ * This file is part of Satunes.
+ *
+ * Satunes is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ *
+ * Satunes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU General Public License for more details.
+ *  You should have received a copy of the GNU General Public License along with Satunes.
+ *
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ * **** INFORMATION ABOUT THE AUTHOR *****
+ * The author of this file is Antoine Pirlot, the owner of this project.
+ * You find this original project on Codeberg.
+ *
+ * My Codeberg link is: https://codeberg.org/antoinepirlot
+ * This current project's link is: https://codeberg.org/antoinepirlot/Satunes
+ */
+
+package io.github.antoinepirlot.satunes.models.listeners
+
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
+import androidx.savedstate.SavedState
+
+/**
+ * @author Antoine Pirlot 20/08/2025
+ */
+object OnDestinationChangedListener : NavController.OnDestinationChangedListener {
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: SavedState?
+    ) {
+        if (controller.previousBackStackEntry != null && destination == controller.previousBackStackEntry!!.destination)
+            controller.popBackStack()
+    }
+}

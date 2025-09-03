45b62632653c54a7e33245d0a7ba8aa287654ec1
Antoine Pirlot <antoinepirlot@proton.me>
2025-09-03 21:13:59 +0200

    Update: add space between buttons
    Signed-off-by: Antoine Pirlot <antoinepirlot@proton.me>

--- app/src/main/java/io/github/antoinepirlot/satunes/ui/components/settings/library/subsonic/SubsonicSettings.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/components/settings/library/subsonic/SubsonicSettings.kt
@@ -23,9 +23,9 @@
 
 package io.github.antoinepirlot.satunes.ui.components.settings.library.subsonic
 
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.SnackbarHostState
@@ -34,6 +34,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import io.github.antoinepirlot.jetpack_libs.components.texts.NormalText
 import io.github.antoinepirlot.satunes.R
@@ -67,13 +68,11 @@ fun SubsonicSettings(
             placeholder = { NormalText(text = "https://example.org") },
             singleLine = true
         )
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
+        Row {
             Button(onClick = { satunesViewModel.resetSubsonicUrl() }) {
                 NormalText(text = stringResource(R.string.cancel))
             }
+            Spacer(modifier = Modifier.size(size = 16.dp))
             Button(onClick = {
                 satunesViewModel.applySubsonicUrl(
                     scope = scope,

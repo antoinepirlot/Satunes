40033a06c91f612345ac805f31de6e0e60427edb
Antoine Pirlot <antoinepirlot@proton.me>
2025-09-27 16:15:33 +0200

    Fix: creating genre in song

--- internet/src/main/java/io/github/antoinepirlot/satunes/internet/subsonic/models/media/SubsonicSong.kt
+++ internet/src/main/java/io/github/antoinepirlot/satunes/internet/subsonic/models/media/SubsonicSong.kt
@@ -54,7 +54,8 @@ internal data class SubsonicSong(
     fun toMusic(subsonicApiRequester: SubsonicApiRequester): Music {
         if(mediaType != SubsonicApiRequester.SONG_MEDIA_TYPE)
             throw IllegalStateException("Can't create not song to music.")
-
+        val genre: Genre = Genre(title = genre)
+        DataManager.addGenre(genre = genre)
         return DataManager.getSubsonicMusic(subsonicId = id)?: Music(
             id = -1, //TODO
             subsonicId = id,
@@ -67,7 +68,7 @@ internal data class SubsonicSong(
             folder = DataManager.getSubsonicRootFolder()!!, //TODO give param of folder
             artist = DataManager.getArtist(subsonicId = artistId)!!,
             album = DataManager.getAlbum(subsonicId = albumId)!!,
-            genre = DataManager.getGenre(title = genre)!!,
+            genre = DataManager.getGenre(title = genre.title)!!,
             uri = "${subsonicApiRequester.url}/${subsonicApiRequester.inUrlMandatoryParams}&id=$id".toUri(),
             displayName = "" //TODO
         )

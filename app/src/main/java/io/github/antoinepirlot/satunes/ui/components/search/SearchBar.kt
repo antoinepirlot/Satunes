c65caa594e021c71c88b2dda9e4afb5362623675
Antoine Pirlot <antoinepirlot@proton.me>
2025-12-13 17:11:00 +0100

    Refactor: extract composables

---
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/components/search/SearchBar.kt
@@ -0,0 +1,104 @@
+/*
+ * This file is part of Satunes.
+ *
+ * Satunes is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ * Satunes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with Satunes.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ * *** INFORMATION ABOUT THE AUTHOR *****
+ * The author of this file is Antoine Pirlot, the owner of this project.
+ * You find this original project on Codeberg.
+ *
+ * My Codeberg link is: https://codeberg.org/antoinepirlot
+ * This current project's link is: https://codeberg.org/antoinepirlot/Satunes
+ */
+
+package io.github.antoinepirlot.satunes.ui.components.search
+
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.platform.SoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import io.github.antoinepirlot.jetpack_libs.components.texts.NormalText
+import io.github.antoinepirlot.satunes.R
+import io.github.antoinepirlot.satunes.data.viewmodels.DataViewModel
+import io.github.antoinepirlot.satunes.data.viewmodels.SearchViewModel
+import io.github.antoinepirlot.satunes.models.SearchChips
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+/**
+ * @author Antoine Pirlot 13/12/2025
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchBar(
+    modifier: Modifier = Modifier,
+    searchViewModel: SearchViewModel = viewModel(),
+    dataViewModel: DataViewModel = viewModel(),
+) {
+    val query: String = searchViewModel.query
+    val selectedSearchChips: List<SearchChips> = searchViewModel.selectedSearchChips
+    val searchCoroutine: CoroutineScope = rememberCoroutineScope()
+
+    var searchJob: Job? = null
+    LaunchedEffect(key1 = query, key2 = selectedSearchChips.size) {
+        if (searchJob != null && searchJob!!.isActive) {
+            searchJob!!.cancel()
+        }
+        searchJob = searchCoroutine.launch {
+            searchViewModel.search(
+                dataViewModel = dataViewModel,
+                selectedSearchChips = selectedSearchChips,
+            )
+        }
+    }
+
+    val focusRequester: FocusRequester = remember { FocusRequester() }
+    LaunchedEffect(key1 = true) {
+        // Request focus after composable becomes visible
+        focusRequester.requestFocus()
+    }
+
+    val keyboard: SoftwareKeyboardController? = LocalSoftwareKeyboardController.current
+
+    SearchBar(
+        modifier = modifier.focusRequester(focusRequester),
+        inputField = {
+            SearchBarDefaults.InputField(
+                modifier = Modifier.focusRequester(focusRequester),
+                query = query,
+                onQueryChange = { searchViewModel.updateQuery(value = it) },
+                onSearch = {
+                    searchViewModel.updateQuery(value = it)
+                    keyboard?.hide()
+                },
+                placeholder = { NormalText(text = stringResource(id = R.string.search_placeholder)) },
+                expanded = false,
+                onExpandedChange = { /* Do not use expanded mode */ },
+            )
+        },
+        expanded = false,
+        onExpandedChange = { /* Do not use expanded mode */ },
+        windowInsets = WindowInsets(top = 0.dp), //Remove top padding of search bar introduced in API 35 (Android 15 Vanilla Ice Cream)
+        content = { /* Content if expanded is true but never used */ }
+    )
+}

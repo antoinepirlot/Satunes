e9efd13fdafa666741aea23d117ec68366f4dcf2
Antoine Pirlot <antoinepirlot@proton.me>
2025-12-13 17:51:34 +0100

    Remove: spacer as it is no more needed

--- app/src/main/java/io/github/antoinepirlot/satunes/ui/views/search/SearchView.kt
+++ app/src/main/java/io/github/antoinepirlot/satunes/ui/views/search/SearchView.kt
@@ -22,9 +22,7 @@ package io.github.antoinepirlot.satunes.ui.views.search
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -32,7 +30,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import io.github.antoinepirlot.satunes.data.states.SearchUiState
 import io.github.antoinepirlot.satunes.data.viewmodels.SearchViewModel
@@ -57,7 +54,6 @@ internal fun SearchView(
         verticalArrangement = Arrangement.Top,
     ) {
         SearchBar()
-        Spacer(modifier = Modifier.size(16.dp))
         SearchModeTabSelector()
         searchUiState.selectedSection.composable()
     }

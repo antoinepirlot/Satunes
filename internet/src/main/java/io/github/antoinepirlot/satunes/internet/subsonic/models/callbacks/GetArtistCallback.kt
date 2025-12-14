7d2984eff6a4f451bdb40ce2e76665134396769e
Antoine Pirlot <antoinepirlot@proton.me>
2025-12-14 16:00:15 +0100

    Fix: missing getArtist callback

---
+++ internet/src/main/java/io/github/antoinepirlot/satunes/internet/subsonic/models/callbacks/GetArtistCallback.kt
@@ -0,0 +1,60 @@
+/*
+ * This file is part of Satunes.
+ *
+ * Satunes is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version.
+ * Satunes is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with Satunes.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ * *** INFORMATION ABOUT THE AUTHOR *****
+ * The author of this file is Antoine Pirlot, the owner of this project.
+ * You find this original project on Codeberg.
+ *
+ * My Codeberg link is: https://codeberg.org/antoinepirlot
+ * This current project's link is: https://codeberg.org/antoinepirlot/Satunes
+ */
+
+package io.github.antoinepirlot.satunes.internet.subsonic.models.callbacks
+
+import io.github.antoinepirlot.satunes.database.models.media.subsonic.SubsonicArtist
+import io.github.antoinepirlot.satunes.internet.subsonic.SubsonicApiRequester
+import io.github.antoinepirlot.satunes.internet.subsonic.models.responses.Error
+import okhttp3.Call
+import okhttp3.Response
+
+/**
+ * @author Antoine Pirlot 14/12/2025
+ */
+internal class GetArtistCallback(
+    subsonicApiRequester: SubsonicApiRequester,
+    private val onDataRetrieved: (SubsonicArtist) -> Unit,
+    onSucceed: (() -> Unit)? = null,
+    onFinished: (() -> Unit)? = null,
+    onError: ((Error?) -> Unit)? = null,
+) : SubsonicCallback(
+    subsonicApiRequester = subsonicApiRequester,
+    onSucceed = onSucceed,
+    onFinished = onFinished,
+    onError = onError
+) {
+    override fun onResponse(call: Call, response: Response) {
+        super.onResponse(call, response)
+        if (this.processData())
+            this.onSucceed?.invoke()
+        this.onFinished?.invoke()
+    }
+
+    private fun processData(): Boolean {
+        if (this.subsonicResponse == null) return false
+        if (this.subsonicResponse!!.artist == null) return false
+        onDataRetrieved(
+            this.subsonicResponse!!.artist!!
+                .toSubsonicMedia(subsonicApiRequester = subsonicApiRequester) as SubsonicArtist
+        )
+        return true
+    }
+}
